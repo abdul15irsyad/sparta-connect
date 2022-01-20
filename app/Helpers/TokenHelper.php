ea08339a68da616c346e87130480473d180cbb29
Irsyad Abdul <abdulirsyad15@gmail.com>
2022-01-21 04:32:35 +0700

    fix bugs

--- app/Helpers/TokenHelper.php
+++ app/Helpers/TokenHelper.php
@@ -21,25 +21,4 @@ class TokenHelper
 
         return $token;
     }
-
-    public static function check_token($token, $route)
-    {
-        // if token invalid
-        if (!$token) {
-            return redirect()
-                ->route($route)
-                ->with('type', 'warning')
-                ->with('message', 'invalid link');
-        }
-
-        // if token expired
-        if (Carbon::now()->gt($token->expired_at)) {
-            $token->status = 0;
-            $token->save();
-            return redirect()
-                ->route($route)
-                ->with('type', 'warning')
-                ->with('message', 'link expired, please make a new request');
-        }
-    }
 }
